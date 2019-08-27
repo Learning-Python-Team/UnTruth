1136fe98a7bac77f6235133c1720a62501eb94dc
mphirke <margeshphirke@gmail.com>
2019-08-27 06:49:22 +0530

    Update headline.py
    Removed redudancy.

--- modules/headline.py
+++ modules/headline.py
@@ -1,9 +1,7 @@
 import urllib.request
-from bs4 import BeautifulSoup
 from textblob.classifiers import NaiveBayesClassifier
 from textblob import TextBlob
 import newspaper
-import nltk
 
 class title:
 
@@ -11,22 +9,11 @@ class title:
     def __init__(self): 
         self.news_url="https://edition.cnn.com/2019/08/25/politics/trump-g7-boris-johnson-emmanuel-macron/index.html"
 
-
     def extract_headline(self):
-        self.net_con=True #Expecting Internet Connection to be working initially
-        
-        try:
-            article = newspaper.Article(self.news_url)
-            article.download()
-            article.parse()
-        
-        except urllib.error.URLError:
-            print("\nCONNECTIION ERROR:There may be a connection problem. Please check if the device is connected to the Internet")
-            self.net_con=False #Value update if the program is unable to connenct
-            article.title = "Invalid URL/Could not extract title"
+        article = newspaper.Article(self.news_url)
+        article.download()
+        article.parse()
         return article.title.strip()
-
-
 
     #Adding Training Data
     def train_data(self, headline):
@@ -37,10 +24,7 @@ class title:
                 return sentiment
 
         except:
-            if self.net_con==False:
-                pass
-            else:
-                print("\n\nProgram Error")
+            print("\n\n Connection/Program Error")
 
 
     def headline_category(self,headline,sentiment):
